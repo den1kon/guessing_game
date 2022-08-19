f63c5e6dc312a782467713b3e134f7772e39f933
den1k0n <dkondratiuk06@gmail.com>
2022-08-19 22:02:46 +0300

    upgrade guessing game

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,57 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    // println!("The secret number is {secret_number}");
+    println!("You are playing 'Guess the number!'");
     
+    println!("The secret number lies in range between x and y");
+
+    println!("Enter x:");
+    
+    let mut min = String::new();
+
     loop {
-        println!("Please input your guess.");
+        io::stdin().read_line(&mut min).expect("Failed to read line");
+        let  min: u32 = match min.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a valid number!");
+                continue;
+            },
+        };
+        break;
+    };
+
+
+    println!("Enter y:");
+
+    let mut max = String::new();
+
+    loop {
+        io::stdin().read_line(&mut max).expect("Failed to read line");
+        let max: u32 = match max.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a valid number!");
+                continue;
+            },
+        };
+        break;
+    };
+
+    let x: u32 = min.trim().parse().expect("");
+    let y: u32 = max.trim().parse().expect("");
+
+    let secret_number = rand::thread_rng().gen_range(x..=y);
+
+    //println!("The secret number is {secret_number}");
+    
+    println!("Enter number of attempts:");
+    let mut attempts = String::new();
+    io::stdin().read_line(&mut attempts).expect("Failed to read line");
+    let attempts: u32 = attempts.trim().parse().expect("");
+
+    for i in 1..=attempts {
+        println!("Please input your guess. Attempt: {i}");
 
         let mut guess = String::new();
 
@@ -30,8 +73,9 @@ fn main() {
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
                 println!("You win");
-                break;
+                return;
             }
         }
-    }
+    };
+    println!("You've ran out of attempts. You lose!");
 }
